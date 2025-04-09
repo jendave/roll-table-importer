6f4073e2ad47ecf29bc5587f9f25cd309ba04aea
David Hudson <david.hudson@disney.com>
2025-04-09 16:51:20 -0700

    remove non-rolltable stuff

--- src/module/renderSidebarButtons.ts
+++ src/module/renderSidebarButtons.ts
@@ -1,9 +1,6 @@
-import { importActorForm as ImportActorForm } from './importActorForm';
 import { importTableForm as ImportTableForm } from './importTableForm';
-import { importItemForm as ImportItemForm } from './importItemForm';
 
 import { Handler } from './importForm';
-import { importJournalForm as ImportJournalForm } from './importJournalForm';
 
 export function renderSidebarButtons(settings: Settings, tab: string, handler: Handler) {
   if (settings.id != tab) return;
@@ -17,23 +14,8 @@ export function renderSidebarButtons(settings: Settings, tab: string, handler: H
   html.find('#inputButton').on('click', async (e) => {
     e.preventDefault();
     switch (tab) {
-      case 'journal': {
-        const form = new ImportJournalForm({ handler, tab });
-        form.render(true);
-        break;
-      }
       case 'tables': {
         const form = new ImportTableForm(handler, tab);
-        form.render(true);
-        break;
-      }
-      case 'actors': {
-        const form = new ImportActorForm({ handler, tab });
-        form.render(true);
-        break;
-      }
-      case 'items': {
-        const form = new ImportItemForm({ handler, tab });
         form.render(true);
         break;
       }
