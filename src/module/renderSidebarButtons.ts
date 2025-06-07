37b74982562436bf3748a42b783329336349f4fd
David Hudson <david.hudson@disney.com>
2025-06-06 23:53:55 -0700

    improvement

--- src/module/renderSidebarButtons.ts
+++ src/module/renderSidebarButtons.ts
@@ -5,7 +5,7 @@ import { Handler } from './importForm';
 export function renderSidebarButtons(settings: Settings, tab: string, handler: Handler) {
   if (settings.id != tab) return;
   const name = tab.charAt(0).toUpperCase() + tab.slice(1);
-  const html = settings.element;
+  const html = $(settings.element);
   if (html.find('#inputButton').length !== 0) return;
   const button = `<button id="inputButton" style="flex-basis: auto;">
   <i class="fas fa-atlas"></i> Import ${name}
